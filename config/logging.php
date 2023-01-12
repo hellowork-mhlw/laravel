c45cd2977626c02cc6b27ad11dd2a16202635ff9
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-01-13 01:00:00 +0900

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -70,6 +70,13 @@ return [
             'days' => 14,
         ],
 
+        'chrome' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/chrome.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
