9341260721c31653188fd27d82106f53c81ca282
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-01-17 21:38:42 +0900

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+var_dump('GITHUB_ACTIONS1');
 if (env('GITHUB_ACTIONS')) {
     var_dump('GITHUB_ACTIONS exists');
 }
